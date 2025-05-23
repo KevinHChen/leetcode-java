d141d4b849a15b6ec3ec91fe9f468aefb79fe81d
KevinHChen <greathongjian@yahoo.com>
2025-05-23 17:24:14 +1200

    Update 46. Permutations.java

--- 46. Permutations.java
+++ 46. Permutations.java
@@ -1,5 +1,36 @@
 /**
- * Still a DFS, but add a used array to avoid .contains() check, >90%
+ * Solution 3: similar to solution 2, but avoid 'path' clone, >90%
+ **/
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        results = new ArrayList();
+        boolean[] used = new boolean[nums.length];
+        backtrack(used, nums, new ArrayList<Integer>());
+        return results;
+    }
+
+    private List<List<Integer>> results;
+
+    private void backtrack(boolean[] used, int[] nums, List<Integer> path) {
+        if (path.size() == nums.length) {
+            results.add(new ArrayList<Integer>(path));
+        }
+
+        for (int i = 0; i < nums.length; ++i) {
+            if (used[i] == true)
+                continue;
+
+            path.add(nums[i]);
+            boolean[] used2 = used.clone();
+            used2[i] = true;
+            backtrack(used2, nums, path);
+            path.remove(path.size()-1);
+        }
+    }
+}
+
+/**
+ * Solution 2: Still a DFS, but add a used array to avoid .contains() check, >90%
  **/
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
@@ -30,7 +61,7 @@ class Solution {
 }
 
 /**
- * A really slow DFS solution (2%)
+ * Solution 1: A really slow DFS solution (2%)
  **/
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
