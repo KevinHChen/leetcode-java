9d2999f6e0b305fbf8f01e62cc47f44e0ea673e2
KevinHChen <greathongjian@yahoo.com>
2025-05-30 20:13:49 +1200

    Create 81. Search in Rotated Sorted Array II.java

---
+++ 81. Search in Rotated Sorted Array II.java
@@ -0,0 +1,35 @@
+/**
+ * Solution 1: too much patching. Fast
+ */
+class Solution {
+    public boolean search(int[] nums, int target) {
+        return backtrack(nums, 0, nums.length - 1, target) != -1;
+    }
+
+    private int backtrack(int[] nums, int left, int right, int target) {
+        while (left < right - 1 && nums[left] == nums[right] && nums[left] != target) {
+            left++;
+            right--;
+        }
+        if (left > right)
+            return -1;
+
+        int mid = (left + right) / 2;
+        if (nums[mid] == target)
+            return mid;
+
+        if (nums[left] <= nums[mid]) { // left half is ordered
+            if (nums[left] <= target && target < nums[mid]) {
+                return backtrack(nums, left, mid - 1, target);
+            } else {
+                return backtrack(nums, mid + 1, right, target);
+            }
+        } else { // right half is ordered
+            if (nums[mid] < target && target <= nums[right]) {
+                return backtrack(nums, mid + 1, right, target);
+            } else {
+                return backtrack(nums, left, mid - 1, target);
+            }
+        }
+    }
+}
